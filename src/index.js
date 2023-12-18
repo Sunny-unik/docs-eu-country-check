52b8180316a224c7fc72f9c8ab5afb114ad8b8f5
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-18 22:07:19 +0530

    feat: add views

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import path from "path";
+import fs from "fs";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "./views/App";
 
 config();
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "assets")));
 
 app.get("/health", (_req, res) => res.send("OK"));
+
+app.get("/", (_req, res) => {
+  fs.readFile(path.resolve("public/index.html"), "utf8", (err, data) => {
+    if (err) return res.status(500).send("Internal Server Error");
+
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+  });
+});
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
