3fbc1afe5a3bbcb1725288cf4294d8d1589562e2
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-20 23:51:59 +0530

    feat: add navbar

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   const [clientMessage, setClientMessage] = useState("");
@@ -8,10 +9,16 @@ const App = () => {
   }, []);
 
   return (
-    <div className="container">
-      <h1>Hello World!</h1>
-      <h2>{clientMessage}</h2>
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className="container">
+        <h1>Hello World!</h1>
+        <h2>{clientMessage}</h2>
+      </main>
+      <footer></footer>
+    </>
   );
 };
 
---
+++ src/views/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar container">
+      <div className="navbar-brand">
+        <a href="/">dashboard</a>
+      </div>
+      <ul className="nav-items">
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+        <li className="nav-item">
+          <a href="/#">some</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
