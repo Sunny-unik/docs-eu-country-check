fab2d1c520f5c512e0ae35905f27a8871c69a5d4
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2023-12-31 01:21:52 +0530

    feat: update github-buttons & layout in footer

--- src/views/components/Footer.jsx
+++ src/views/components/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import NewTabIcon from "./NewTabIcon";
+import SimpleIcons from "./SimpleIcons";
+import GitHubButton from "react-github-btn";
 
 export default function Footer() {
   return (
-    <footer className="dark w-100 vertical-space">
+    <footer className="dark w-100 vertical-space flex column justify-content-between">
       <div className="flex container justify-content-between">
-        <div>
-          <h3>Documentation</h3>
+        <div className="box">
+          <h3 className="text-muted helvetica">Documentation</h3>
           <ul>
             <li>
               <NavLink to="/docs/install">Installation</NavLink>
@@ -17,11 +19,81 @@ export default function Footer() {
             </li>
           </ul>
         </div>
-        <div>something</div>
+
+        <div className="box">something</div>
+      </div>
+
+      <div className="box text-center">
+        <div>
+          <a
+            href="https://github.com/sunny-unik"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SimpleIcons
+              name="github"
+              alt="github profile link"
+              width="20"
+              height="20"
+              color="edf2f5"
+              darkModeColor="edf2f5"
+            />
+          </a>
+          <GitHubButton
+            href="https://github.com/sunny-unik/eu-country-check"
+            data-color-scheme="no-preference: light; light: dark; dark: light;"
+            data-icon="octicon-star"
+            aria-label="Star Sunny-unik/eu-country-check on GitHub"
+          >
+            Star
+          </GitHubButton>
+          <a
+            href="https://www.linkedin.com/in/sunny-gandhwani"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SimpleIcons
+              name="linkedin"
+              alt="linkedin profile link"
+              width="20"
+              height="20"
+              color="edf2f5"
+              darkModeColor="edf2f5"
+            />
+          </a>
+          <GitHubButton
+            href="https://github.com/sunny-unik/eu-country-check/issues"
+            data-color-scheme="no-preference: light; light: dark; dark: light;"
+            data-icon="octicon-issue-opened"
+            aria-label="Issue in Sunny-unik/eu-country-check on GitHub"
+          >
+            Issue
+          </GitHubButton>
+          <a
+            href="https://twitter.com/SunnyUnik"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SimpleIcons
+              name="twitter"
+              alt="twitter profile link"
+              width="20"
+              height="20"
+              color="edf2f5"
+              darkModeColor="edf2f5"
+            />
+          </a>
+        </div>
+
         <div>
           Copyright © 2023{" "}
-          <a href="">
-            Sunny Gandhwani <NewTabIcon />{" "}
+          <a
+            href="https://sunny-unik.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sunny Gandhwani
+            <NewTabIcon />
           </a>
         </div>
       </div>
