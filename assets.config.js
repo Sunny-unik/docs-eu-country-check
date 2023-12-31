11f01e60c1c32f42b09f2dad5cbde0907f6c8405
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-01-01 01:47:43 +0530

    feat: add chokidar to watch public/

--- assets.config.js
+++ assets.config.js
@@ -1,11 +1,26 @@
-const { copy } = require("fs-extra");
+const fsExtra = require("fs-extra");
+const chokidar = require("chokidar");
 
-copy(
-  "public",
-  "dist/assets",
-  { filter: (src) => src !== "public\\index.html" },
-  (error) => {
-    if (error) return console.log("Error in copy public directory");
-    console.log("Public directory copied successfully");
-  }
-);
+function copyAssets(
+  sourcePath = "public",
+  destination = "dist/assets",
+  filter = (src) => src !== "public\\index.html",
+  operation = "copy"
+) {
+  fsExtra[operation](sourcePath, destination, { filter }, (error) => {
+    if (error) return console.error("Error in update assets directory");
+    console.log("Public directory updated successfully");
+  });
+}
+copyAssets();
+
+if (process.env.NODE_ENV === "development") {
+  console.log("Chokidar keep eyes on Public directory");
+  chokidar
+    .watch("./public")
+    .on("add", (path) => copyAssets())
+    .on("change", (path, stats) => console.log("change", path, stats))
+    .on("unlink", (path) => console.log("unlink", path))
+    .on("addDir", (path) => console.log("addDir", path))
+    .on("unlinkDir", (path) => console.log("unlinkDir", path));
+}
